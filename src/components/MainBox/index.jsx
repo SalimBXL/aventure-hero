8abbf2207c050d8df0b0f34a1ca63964740e9b87
SalimBXL <el.salim.salim@gmail.com>
2023-01-06 16:52:03 +0100

    adventure-hero init

---
+++ src/components/MainBox/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./MainBox.css";
+
+function MainBox() {
+  return (<div className='MainBox'>
+    <img src="https://www.w3schools.com/css/rock600x400.jpg" alt="Illustration" id='MainBox-illustration' />
+    <div id="MainBox-content">
+        <h1>TEST</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel semper velit. Quisque vestibulum felis vel felis mattis, nec porta libero fermentum. Suspendisse sagittis euismod eleifend. Proin consequat justo id lorem auctor gravida. Nam diam quam, sodales sed velit non, interdum consectetur leo. Aenean augue ipsum, ornare sed feugiat vel, tristique a tortor. Pellentesque dui felis, volutpat vel convallis eu, hendrerit a ex. Nulla maximus ultrices porttitor. Aliquam eu molestie eros, id consequat elit. Fusce dapibus fermentum urna, eget ultricies ante mattis eu. Praesent sed mi nec ante volutpat blandit ut ac nisi. Integer lobortis ipsum tellus, quis interdum dui semper at. Curabitur sed nibh lacus. Ut vulputate ligula eget arcu congue, non varius ante tincidunt.</p>
+        <p>Ut laoreet dui sed lacinia sodales. Quisque tristique turpis a libero pulvinar feugiat. Ut gravida nibh vel massa rhoncus mollis. Cras eros magna, tristique non dui vitae, tristique fermentum ex. Aliquam ut eleifend metus. Vestibulum ultrices eu lacus quis dictum. Morbi posuere libero ut mi ullamcorper, eu ultricies tellus porta. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec eu ex risus. Quisque et sagittis dui, laoreet convallis diam.</p>
+        <p>Nulla facilisi. Sed sed fringilla ante. Nam sed ante in lorem luctus condimentum sed eget lectus. Proin id laoreet est. Integer pharetra lacinia egestas. Duis blandit nisl diam, eget gravida nunc efficitur posuere. Sed rutrum eget lorem nec dignissim. Morbi pellentesque odio libero, eu blandit sapien placerat venenatis. Nulla porttitor tincidunt interdum. Quisque vel vehicula nibh. Ut cursus condimentum felis, vel aliquet tellus rutrum vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam nulla mauris, venenatis a gravida vel, maximus non odio.</p>
+    </div>
+  </div>);
+}
+
+export default MainBox;
---
+++ src/components/MainMenu/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./MainMenu.css";
+
+function MainMenu() {
+  return (<div className='MainMenu'>
+    
+    <button className='MainMenu-boutton'>MENU 1</button>
+    <button className='MainMenu-boutton'>MENU 2</button>
+    <button className='MainMenu-boutton'>MENU 3</button>
+
+
+  </div>)
+}
+
+export default MainMenu;
